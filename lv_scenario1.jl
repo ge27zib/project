9fabf5aa2cd607afd2c0b4bbe9c7f806015aa661
Aditya Sahu <ge27zib@tum.de>
2025-02-14 17:53:36 +0100

    lv scenario 1 completed

--- lv_scenario1.jl
+++ lv_scenario1.jl
@@ -7,6 +7,7 @@ using Optimisers
 using SciMLSensitivity 
 using Statistics
 using Random 
+using LinearAlgebra
 using Printf
 using ParameterSchedulers 
 using JLD2
@@ -39,7 +40,7 @@ end
 Flux.@functor NDE
 Optimisers.trainable(m::NDE) = (p = m.p,)
 
-# Generate dataset
+# Generate the model dataset
 function lotka_volterra!(du, u, p, t)
     x, y = u
     α, β, γ, δ = p 
@@ -58,30 +59,8 @@ u0 = Float32.([0.44249296, 4.6280594])
 prob = ODEProblem(lotka_volterra!, u0, tspan, p)
 sol = solve(prob, Tsit5(), saveat=dt)
 
-# Make dataset sparse
-function sparsify_data(sol; fraction=1.)
-    t = sol.t
-    X = Array(sol)       
-    
-    mask = rand(length(t)) .< fraction 
-    y_sparse = X[2, :] .* mask # By default 0% of the values are missing
-    return t, X, y_sparse, mask
-end
-
-Random.seed!(1234)
-t, X, y_sparse, mask = sparsify_data(sol)
-
-# Pack the full state into one matrix
-sol_sparse = vcat(sol[1, :]', y_sparse')
-
 # Create overlapping batches
-train, valid = NODEDataloader(sol_sparse, t, 2; valid_set=0.2) # Batch size is 2
-
-# Extract mask slice for a given batch's time vector
-function get_mask_for_batch(batch_t, global_mask, t0, dt)
-    indices = round.(Int, (batch_t .- t0) ./ dt .+ 1)
-    return global_mask[indices]
-end
+train, valid = NODEDataloader(sol, 2; dt=dt, valid_set=0.2)
 
 # plot(train.data') # Visualize the data
 
@@ -92,14 +71,14 @@ U_nn = Chain(
     Dense(32, 2))
 θ, U_re = Flux.destructure(U_nn)
 
-p = θ # Learnable parameters
+p_ln = θ # Learnable parameters
 
 # Define neural ODE
-function neural_ode(u, p, t)
+function neural_ode(u, p_ln, t)
     x, y = u
     α_const = Float32(α)
     γ_const = Float32(γ)
-    θ = p
+    θ = p_ln
     U = U_re(θ)(u)
     
     dx = α_const*x + U[1]   
@@ -108,25 +87,16 @@ function neural_ode(u, p, t)
 end
 
 # Create the NODE problem and model
-node_prob = ODEProblem(neural_ode, u0, (Float32(0.), Float32(dt)), p)
+node_prob = ODEProblem(neural_ode, u0, (Float32(0.), Float32(dt)), p_ln)
 model = NDE(node_prob; reltol=1f-5, dt=dt)
 
 # Define the loss function
-function loss(m, batch, truth, batch_mask; λ1=0f0, λ2=1f-5)
-    pred = m(batch)
-    pred_x = pred[1, :]
-    pred_y = pred[2, :]
-    truth_x = truth[1, :]
-    truth_y = truth[2, :]
+function loss(m, batch, truth; λ1=0f0, λ2=1f-5)
+    loss = Flux.mse(m(batch), truth) # L2 loss
+    reg1 = λ1 * sum(abs, m.p) # L1 regularization
+    reg2 = λ2 * sum(abs2, m.p) # L2 regularization
     
-    loss_x = Flux.mse(pred_x, truth_x)
-    loss_y = Flux.mse(pred_y .* batch_mask, truth_y .* batch_mask) # Loss is calculated only on the observed points
-    
-    # Reguralization
-    reg1 = λ1 * sum(abs, m.p)
-    reg2 = λ2 * sum(abs2, m.p)
-    
-    return loss_x + loss_y + reg1 + reg2
+    return loss + reg1 + reg2
 end
 
 # Set up the optimiser
@@ -148,7 +118,7 @@ if Train
     
     for i_τ = 2:τ_max
 
-        train_τ = NODEDataloader(sol_sparse, t, i_τ)
+        train_τ = NODEDataloader(sol, i_τ)
         @info "Batch size = $(i_τ)"
     
         for (epoch, η) in zip(1:epochs, scheduler)
@@ -156,14 +126,13 @@ if Train
 
             for batch in train_τ
                 batch_t, batch_data = batch
-                batch_mask = get_mask_for_batch(batch_t, mask, t[1], dt)
                 Flux.train!(model, [(batch_t, batch_data)], opt_state) do m, bt, bd
-                    loss(m, (bt, bd), bd, batch_mask)
+                    loss(m, (bt, bd), bd)
                 end
             end
             
-            train_loss = mean([loss(model, train[i], train[i][2], get_mask_for_batch(train[i][1], mask, t[1], dt)) for i=1:NN_train])
-            valid_loss = mean([loss(model, valid[i], valid[i][2], get_mask_for_batch(valid[i][1], mask, t[1], dt)) for i=1:NN_valid])
+            train_loss = mean([loss(model, train[i], train[i][2]) for i=1:NN_train])
+            valid_loss = mean([loss(model, valid[i], valid[i][2]) for i=1:NN_valid])
         
             @printf("Epoch %3d | LR: %.2e | Train Loss: %.4f | Valid Loss: %.4f\n", epoch, η, train_loss, valid_loss)
 
@@ -171,40 +140,66 @@ if Train
     end
 end
 
-# Prediction for the first 70 points
+# # Save the model
+# jldsave("lv_scenario1.jld2"; model=model, model_params=model.p)
+
+# Load model and parameters
+saved_data = load("lv_scenario1.jld2")
+model = saved_data["model"]
+p_ln = saved_data["model_params"]
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------
+############################################ Prediction for the NODE: We visualize for the first 70 points #####################################
+# ----------------------------------------------------------------------------------------------------------------------------------------------
 
 # Trajectories
-y_sp = sol_sparse[2, 1:70]
-y_spskip = y_sp[y_sp .!= 0] # True nonzero y values
+t = sol.t
+x = Array(sol)  # Ground truth 
+pred = model((t, x))[:, 1:70]
 
-pred = model((t, sol_sparse))[:, 1:70]
-plot(pred')
-plot!(sol_sparse[1, 1:70], seriestype = :scatter, legend = false)
-plot!(y_spskip, seriestype = :scatter, legend = false)
+plot(pred[1, 1:70], label="UDE Approximation", lw=2, color=:red, legend=:topleft)
+plot!(pred[2, 1:70], label="", lw=2, color=:red)
+scatter!(x[1, 1:70], label="Measurements", marker=:circle, color=:black)
+scatter!(x[2, 1:70], label="", marker=:circle, color=:black)
 
-# Interaction terms
-Uxy_pred = U_re(model.p.θ)(sol_sparse)[:, 1:70]
-plot(Uxy_pred')
+xlabel!("t")
+ylabel!("x(t), y(t)")
 
-Uxy_truth1 = -(β.*sol[1, :].*sol[2, :] )[1:70]
-Uxy_truth2 = (δ.*sol[1, :].*sol[2, :])[1:70]
+# Interaction Terms
+Uxy_pred = U_re(p_ln)(x)[:, 1:70]
+
+plot(Uxy_pred[1, 1:70], label="UDE Approximation", lw=2, color=:red, legend=:topleft)
+plot!(Uxy_pred[2, 1:70], label="", lw=2, color=:red)
+
+Uxy_truth1 = -(β*x[1, :].*x[2, :])[1:70]
+Uxy_truth2 = (δ*x[1, :].*x[2, :])[1:70]
 Uxy_truth = vcat(Uxy_truth1', Uxy_truth2')
-plot!(Uxy_truth')
 
-# # Save the model
-# jldsave("lv_no_sparse_gamma_fixed.jld2"; model=model, model_params=model.p)
+plot!(Uxy_truth[1, 1:70], label="True Interaction", lw=2, color=:black)
+plot!(Uxy_truth[2, 1:70], label="", lw=2, color=:black)
 
-# # Load model and parameters
-# saved_data = load("lv_no_sparse_gamma_fixed.jld2")
-# model = saved_data["model"]
-# θ = saved_data["model_params"]
+xlabel!("t")
+ylabel!("U(x,y)")
+
+# L2 Loss
+train_70 = NODEDataloader(sol, 70)
+batch_t, batch_data = train_70[1]  
+pred_70 = model((batch_t, batch_data))
+
+l2_error = [norm(pred_70[:, i] - batch_data[:, i]) for i in 1:length(batch_t)] # Compute L2 error at each time step
+l2_error = max.(l2_error, eps()) # Prevent log(0)
+
+y_ticks = 10.0 .^ (-3.5:1.:0) # Custom ticks
+y_labels = ["10^{$(round(y, digits=1))}" for y in log10.(y_ticks)] # Format labels as 10^x
+
+plot(batch_t, l2_error, xlabel="t", ylabel="L2-Error", xlims=(0, batch_t[end]), yscale=:log10, yticks=(y_ticks, y_labels),  ylims=(minimum(y_ticks), maximum(y_ticks)), lw=2, color=:red, legend=false)
 
 #---------------------------------------------------------------------------------------------------------------------------------------------
 #################################################### Finding the Symbolic Expression #########################################################
 #---------------------------------------------------------------------------------------------------------------------------------------------
 
 x = Array(sol) # Ground truth
-mlp = U_re(θ)
+mlp = U_re(p_ln)
 dx = mlp(x)
 
 options = SymbolicRegression.Options(
@@ -225,4 +220,4 @@ pareto1[3].tree
 
 pareto2 = calculate_pareto_frontier(x, dx[2, :], hall_of_fame[2], options)
 pareto2[3].tree
-####################################################### Recovered the interaction terms ########################################################
+############################################### Successfully recovered the interaction terms ####################################################
